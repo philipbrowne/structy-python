df4b578413954a91be11f35a2829f5ce72f9ba5b
Philip Browne <pbrowne@gmail.com>
2023-03-11 11:49:27 -0500

    feat: start graph problems

---
+++ 4_graph/has_path.py
@@ -0,0 +1,27 @@
+def has_path_dfi(graph, src, dst): # Depth First Iterative
+    if src not in graph or dst not in graph:
+        return False
+    stack = [src]
+    while stack:
+        current = stack.pop()
+        if current == dst:
+            return True
+        for neighbor in graph[current]:
+            stack.append(neighbor)
+    return False
+
+from collections import deque 
+
+def has_path_bfi(graph, src, dst): # Breadth First Iterative
+    if src not in graph or dst not in graph:
+        return False
+    queue = deque([src])
+    while queue:
+        current = queue.popleft()
+        if current == dst:
+            return True
+        for neighbor in graph[current]:
+            queue.append(neighbor)
+    return False
+
+def has_path(graph, src, dst): # Recursive
--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. String Array
 2. Linked List
 3. Binary Tree
+4. Graph
